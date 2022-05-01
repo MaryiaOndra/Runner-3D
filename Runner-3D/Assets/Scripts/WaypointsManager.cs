b86384d367a2d9490dc9d34f24faf4f7d058d378
MariaOndra <m.ondra@7tam.net>
2022-05-01 19:49:00 +0400

    add waypoins movement

--- Runner-3D/Assets/Scripts/WaypointsManager.cs
+++ Runner-3D/Assets/Scripts/WaypointsManager.cs
@@ -12,17 +12,45 @@ public class WaypointsManager : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float speed;
 
+    private int _nextPositionIndex;
+    private Transform _nextPointTransform;
+    private bool _isFinish;
+
+    private void Start()
+    {
+        waypoints.Insert(0, StartWaypoint);
+        waypoints.Insert(waypoints.Count, FinishWaypoint);
+        _nextPointTransform = StartWaypoint.transform;
+        player.transform.position = _nextPointTransform.position;
+    }
+
     private void FixedUpdate()
     {
-        MovePlayer();
+        if (Input.GetKey(KeyCode.Mouse0) && !_isFinish)
+        {
+            MovePlayer();
+        }
     }
 
     private void MovePlayer()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Vector3.Distance(player.transform.position, _nextPointTransform.position)
+            <= 0.1f)
         {
-            player.position += Vector3.forward * Time.deltaTime * speed;
-            // character.Move(Vector3.forward * Time.deltaTime * speed); 
+            _nextPositionIndex++;
+
+            if (_nextPositionIndex > waypoints.Count)
+            {
+                _isFinish = true;
+                Debug.Log("YOU WIN!");
+            }
+            _nextPointTransform = waypoints[_nextPositionIndex].gameObject.transform;
+        }
+        else
+        {
+            player.position = Vector3.MoveTowards(player.transform.position,
+                _nextPointTransform.position, speed * Time.deltaTime);
+            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.LookRotation(_nextPointTransform.position), Time.deltaTime * 10f);
         }
     }
 }
